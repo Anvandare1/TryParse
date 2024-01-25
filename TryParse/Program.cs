7a6f053e2b2876e92595bf2b9b768c9c26994099
Anvandare1 <90597251+Anvandare1@users.noreply.github.com>
2024-01-25 11:23:59 +0100

    gsagag
    gag

---
+++ TryParse/Program.cs
@@ -0,0 +1,19 @@
+﻿int resultat;
+if(!int.TryParse(Console.ReadLine(), out resultat))
+   Console.WriteLine("Skriv in en siffra");
+
+    static bool TryParse(string s, out int result)
+   {
+       char[] characters = s.ToCharArray();
+
+       foreach(char c in characters)
+       {
+          if(!char.IsDigit(c))
+          {
+            result = 0;
+            return false;
+          }
+       }
+       result = int.Parse(s);
+       return true;
+   }
